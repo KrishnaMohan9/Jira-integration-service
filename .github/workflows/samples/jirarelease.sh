f510562f3647aecdbbff71164211d374be20dece
KrishnaMohan9 <89584451+KrishnaMohan9@users.noreply.github.com>
2023-06-16 11:45:30 +1000

    Update jirarelease.sh

--- .github/workflows/samples/jirarelease.sh
+++ .github/workflows/samples/jirarelease.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-content=$(curl --location  ${RESTAPI} \
+content=$(curl --location  $RESTAPI \
 --header 'Content-Type: application/json' \
---header 'Authorization: Basic ${TOKEN}'| jq -r '.values[].id'
+--header 'Authorization: Basic $TOKEN'| jq -r '.values[].id')
 
 report="Sonar Report"
 testing="communication"
@@ -10,6 +10,6 @@ testing="communication"
 curl --location 'https://devopsprodemo.atlassian.net/gateway/api/graphql' \
 --header 'X-ExperimentalApi:  AddRelatedWorkToVersion' \
 --header 'Content-Type: application/json' \
---header 'Authorization: Basic  ${TOKEN}' \
+--header 'Authorization: Basic  $TOKEN' \
 --data '{"query":"mutation useAddGenericLinkRelatedWork_addRelatedWorkMutation($versionId: ID!, $relatedWorkId: ID!, $url: URL, $title: String, $category: String!) {\n  jira {\n    addRelatedWorkToVersion(input: {versionId: $versionId, relatedWorkId: $relatedWorkId, url: $url, title: $title, category: $category} ) {\n      success\n      errors {\n        message\n      }\n    }\n  }\n}",
  "variables":{"versionId":"'"$versionid/$content"'","relatedWorkId":"'"$content"'","url":"'"$url/dashboard?id=meda-customer-service"'","title":"'"$report"'","category":"'"$testing"'"}}'
