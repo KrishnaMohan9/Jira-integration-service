7ffa1ee34a16bdf1ebdf626848c1b6dee045c229
KrishnaMohan9 <89584451+KrishnaMohan9@users.noreply.github.com>
2023-06-19 08:05:53 +1000

    Update jirarelease.sh

--- .github/workflows/samples/jirarelease.sh
+++ .github/workflows/samples/jirarelease.sh
@@ -14,4 +14,4 @@ curl --location 'https://devopsprodemo.atlassian.net/gateway/api/graphql' \
 --header 'Content-Type: application/json' \
 --header "Authorization: Basic  $TOKEN" \
 --data '{"query":"mutation useAddGenericLinkRelatedWork_addRelatedWorkMutation($versionId: ID!, $relatedWorkId: ID!, $url: URL, $title: String, $category: String!) {\n  jira {\n    addRelatedWorkToVersion(input: {versionId: $versionId, relatedWorkId: $relatedWorkId, url: $url, title: $title, category: $category} ) {\n      success\n      errors {\n        message\n      }\n    }\n  }\n}",
- "variables":{"versionId":"'"$versionid/$content"'","relatedWorkId":"'"123"'","url":"'"$url/dashboard?id=meda-customer-service"'","title":"'"$report"'","category":"'"$testing"'"}}'
+ "variables":{"versionId":"'"$versionid/$content"'","relatedWorkId":"'"58"'","url":"'"$url/dashboard?id=meda-customer-service"'","title":"'"$report"'","category":"'"$testing"'"}}'
