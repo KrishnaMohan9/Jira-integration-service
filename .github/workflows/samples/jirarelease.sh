f3a9cb663ff6995e115690e785216b4524d15a82
KrishnaMohan9 <89584451+KrishnaMohan9@users.noreply.github.com>
2023-06-29 14:25:12 +1000

    Update jirarelease.sh

--- .github/workflows/samples/jirarelease.sh
+++ .github/workflows/samples/jirarelease.sh
@@ -1,17 +1,20 @@
 #!/bin/sh
 
-content=$(curl --location  $RESTAPI \
---header 'Content-Type: application/json' \
---header "Authorization: Basic $TOKEN"| jq -r '.values[].id')
+curl --location 'https://tmrcoral.atlassian.net/rest/api/2/project/10019/version?expand=issuesstatus&maxResults=25&orderBy=-sequence&startAt=0' \
+--header 'Authorization: Basic a3Jpc2huYS5tLmdhbm5hbXJhanVAdG1yLnFsZC5nb3YuYXU6QVRBVFQzeEZmR0YwWEdmUUM4YmQydGI3dzRxbnVNOXFpZFRfLXM5WkhpMkQ5YUlJSTlNdjNwSi0wakpFN1Uyc25PNDQzMU1HT1BTZUFwYVpWZzVJZzNmNTQ2d1pYNWhuVzhZNUZRd3NGRUFfT29yOFh3QVVFTU5fYndfRzZiQk5BODA2Ml8tT2FEbDl6clNUMi0xOHc3Y1lETDh5WnFBMkQ0NHQ0a3dSRnFzSDFpM28zZWJJd1lFPUNFRUM0MDUy' | jq -r '.values[]. name  \
 
-echo $content
+#content=$(curl --location  $RESTAPI \
+#--header 'Content-Type: application/json' \
+#--header "Authorization: Basic $TOKEN"| jq -r '.values[].id')
 
-report="meda-digital-licensing-service-sonarreport"
-testing="licensing-service"
+#echo $content
 
-curl --location 'https://devopsprodemo.atlassian.net/gateway/api/graphql' \
---header 'X-ExperimentalApi:  AddRelatedWorkToVersion' \
---header 'Content-Type: application/json' \
---header "Authorization: Basic  $TOKEN" \
---data '{"query":"mutation useAddGenericLinkRelatedWork_addRelatedWorkMutation($versionId: ID!, $relatedWorkId: ID!, $url: URL, $title: String, $category: String!) {\n  jira {\n    addRelatedWorkToVersion(input: {versionId: $versionId, relatedWorkId: $relatedWorkId, url: $url, title: $title, category: $category} ) {\n      success\n      errors {\n        message\n      }\n    }\n  }\n}",
- "variables":{"versionId":"'"$versionid/$content"'","relatedWorkId":"'"$testing"'","url":"'"$url/dashboard?id=meda-digital-licensing-service"'","title":"'"$report"'","category":"'"$testing"'"}}'
+#report="meda-digital-licensing-service-sonarreport"
+#testing="licensing-service"
+
+#curl --location 'https://devopsprodemo.atlassian.net/gateway/api/graphql' \
+#--header 'X-ExperimentalApi:  AddRelatedWorkToVersion' \
+#--header 'Content-Type: application/json' \
+#--header "Authorization: Basic  $TOKEN" \
+#--data '{"query":"mutation useAddGenericLinkRelatedWork_addRelatedWorkMutation($versionId: ID!, $relatedWorkId: ID!, $url: URL, $title: String, $category: String!) {\n  jira {\n    addRelatedWorkToVersion(input: {versionId: $versionId, relatedWorkId: $relatedWorkId, url: $url, title: $title, category: $category} ) {\n      success\n      errors {\n        message\n      }\n    }\n  }\n}",
+# "variables":{"versionId":"'"$versionid/$content"'","relatedWorkId":"'"$testing"'","url":"'"$url/dashboard?id=meda-digital-licensing-service"'","title":"'"$report"'","category":"'"$testing"'"}}'
