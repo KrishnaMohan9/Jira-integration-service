f44ac5942c3257d42b3e7eaccebc05cc3facb8bb
KrishnaMohan9 <89584451+KrishnaMohan9@users.noreply.github.com>
2023-06-29 15:10:50 +1000

    Update jirarelease.sh

--- .github/workflows/samples/jirarelease.sh
+++ .github/workflows/samples/jirarelease.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
-
-content=$(curl --location 'https://tmrcoral.atlassian.net/rest/api/2/project/10019/version?expand=issuesstatus&maxResults=25&orderBy=-sequence&startAt=0&status=unreleased' \
---header 'Authorization: Basic a3Jpc2huYS5tLmdhbm5hbXJhanVAdG1yLnFsZC5nb3YuYXU6QVRBVFQzeEZmR0YwWEdmUUM4YmQydGI3dzRxbnVNOXFpZFRfLXM5WkhpMkQ5YUlJSTlNdjNwSi0wakpFN1Uyc25PNDQzMU1HT1BTZUFwYVpWZzVJZzNmNTQ2d1pYNWhuVzhZNUZRd3NGRUFfT29yOFh3QVVFTU5fYndfRzZiQk5BODA2Ml8tT2FEbDl6clNUMi0xOHc3Y1lETDh5WnFBMkQ0NHQ0a3dSRnFzSDFpM28zZWJJd1lFPUNFRUM0MDUy' | jq -r '.values[].name')
-
 branch=`git branch --show-current | tr -d "[:space:]" | sed 's:.*/::'`
+content=$(curl --location 'https://tmrcoral.atlassian.net/rest/api/2/project/10019/version?expand=issuesstatus&maxResults=25&orderBy=-sequence&startAt=0&status=unreleased' \
+--header 'Authorization: Basic a3Jpc2huYS5tLmdhbm5hbXJhanVAdG1yLnFsZC5nb3YuYXU6QVRBVFQzeEZmR0YwWEdmUUM4YmQydGI3dzRxbnVNOXFpZFRfLXM5WkhpMkQ5YUlJSTlNdjNwSi0wakpFN1Uyc25PNDQzMU1HT1BTZUFwYVpWZzVJZzNmNTQ2d1pYNWhuVzhZNUZRd3NGRUFfT29yOFh3QVVFTU5fYndfRzZiQk5BODA2Ml8tT2FEbDl6clNUMi0xOHc3Y1lETDh5WnFBMkQ0NHQ0a3dSRnFzSDFpM28zZWJJd1lFPUNFRUM0MDUy' | jq -r '.values[].name' | grep $branch )
 
-#echo $content
-#echo $branch
+#branch=`git branch --show-current | tr -d "[:space:]" | sed 's:.*/::'`
 
-output=$(echo $content | grep '$branch')
+echo $content
 
-echo $output
+
+#output=$(echo $content | grep '$branch')
+
+#echo $output
 
 #content=$(curl --location  $RESTAPI \
 #--header 'Content-Type: application/json' \
